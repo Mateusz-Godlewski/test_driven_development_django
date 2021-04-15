b582918d9bcf83eeba407c9950cdc7c1ad1009be
Mateusz Godlewski <godlewskimateusz98@gmail.com>
2021-04-15 12:25:52 +0100

    Using a unittest in python for a functional test. I could not get the tearDown() to work - with it added the firefox window would close immediately as expected but the test ran for another ~70 seconds. WIthout the tearDown() test were done as soon as the page loaded and asserted that there is no "To-Do" in the title. I've spent around 30 minutes looking for a solution but it didn't go anywhere so I've decided to leave it as is and continue forward. Apparently no one else has ever had an issue with a single, simple test running for 70 seconds. Below is the bash output.
    godle@DESKTOP-TNRKNS3 MINGW64 ~/Desktop/test_driven_dev (master)
    $ python functional_test.py
    F
    ======================================================================
    FAIL: test_can_start_a_list_and_retrieve_it_later (__main__.NewVisitorTest)
    ----------------------------------------------------------------------
    Traceback (most recent call last):
      File "C:\Users\godle\Desktop\test_driven_dev\functional_test.py", line 24, in test_can_start_a_list_and_retrieve_it_later
        self.assertIn("To-Do",  self.browser.title)
    AssertionError: 'To-Do' not found in 'The install worked successfully! Congratulations!'
    
    ----------------------------------------------------------------------
    Ran 1 test in 77.413s
    
    FAILED (failures=1)

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,32 @@
 from selenium import webdriver
+import unittest
 
 geckodriver_path = r"C:\Program Files\Mozilla Firefox\geckodriver.exe"
-browser = webdriver.Firefox(executable_path=geckodriver_path)
-browser.get("http://localhost:8000")
 
-assert "install" in browser.title
+
+#
+class NewVisitorTest(unittest.TestCase):
+
+
+    def setUp(self):
+        # user enters a website
+        self.browser = webdriver.Firefox(executable_path=geckodriver_path)
+
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        self.browser.get("http://localhost:8000")
+        # user notices title
+        self.assertIn("To-Do",  self.browser.title)
+        self.fail("Finish the test")
+
+
+        # user is invited to add a to-do item
+        # after hitting enter user's item shows up on top and the user is invited to add another item
+        # user is notified that URL is being updated after every entry to let them save it and access it later on
+        # specific URL can be accessed whenever and wherever.
+
+
+
+if __name__ == "__main__":
+    unittest.main()
