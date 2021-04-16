469d708e05d299f533bf39ba357f6c97d667fe13
Mateusz Godlewski <godlewskimateusz98@gmail.com>
2021-04-16 12:30:10 +0100

    Expand functional_test.py - it now checks if we can input a to-do item and whetehter it's being saved in a table

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 chromedriver_path = r"C:\Users\godle\Documents\chrome_driver_development\chromedriver.exe"
 
@@ -22,10 +24,32 @@ class NewVisitorTest(unittest.TestCase):
         # user notices title
         self.assertIn("To-Do",  self.browser.title)
 
+        # user notices header
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("To-Do", header_text)
 
         # user is invited to add a to-do item
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(
+            inputbox.get_attribute("placeholder"),
+            "Enter s to-do item"
+        )
+
+        # user writes "Buy feathers"
+        inputbox.send_keys("Buy feathers")
+
         # after hitting enter user's item shows up on top and the user is invited to add another item
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(
+            any(row.text == "Buy feathers" for row in rows)
+        )
+
         # user is notified that URL is being updated after every entry to let them save it and access it later on
+        self.fail("Finish the test")
         # specific URL can be accessed whenever and wherever.
 
 
